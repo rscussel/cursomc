2ca29935e305ac862ad089f91574c814afb1b53c
Ricardo Scussel <scussel.ricardo@gmail.com>
2018-06-12 13:58:10 -0300

    instanciacao categorias

--- src/main/java/com/qsl/cursomc/CursomcApplication.java
+++ src/main/java/com/qsl/cursomc/CursomcApplication.java
@@ -1,12 +1,34 @@
 package com.qsl.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.qsl.cursomc.domain.Categoria;
+import com.qsl.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2= new Categoria(null, "Escritorio");
+
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 }
