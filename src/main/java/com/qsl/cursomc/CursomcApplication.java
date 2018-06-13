1f163289a114aacae7ae41bf8607bd53a68b4e70
Ricardo Scussel <scussel.ricardo@gmail.com>
2018-06-13 16:09:29 -0300

    cliente, telefone, endereco, tipocliente

--- src/main/java/com/qsl/cursomc/CursomcApplication.java
+++ src/main/java/com/qsl/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.qsl.cursomc.domain.Categoria;
 import com.qsl.cursomc.domain.Cidade;
+import com.qsl.cursomc.domain.Cliente;
+import com.qsl.cursomc.domain.Endereco;
 import com.qsl.cursomc.domain.Estado;
 import com.qsl.cursomc.domain.Produto;
+import com.qsl.cursomc.domain.enums.TipoCliente;
 import com.qsl.cursomc.repositories.CategoriaRepository;
 import com.qsl.cursomc.repositories.CidadeRepository;
+import com.qsl.cursomc.repositories.ClienteRepository;
+import com.qsl.cursomc.repositories.EnderecoRepository;
 import com.qsl.cursomc.repositories.EstadoRepository;
 import com.qsl.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepo;
 	@Autowired
 	private CidadeRepository cidadeRepo;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,36 +44,46 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2= new Categoria(null, "Escritorio");
-		
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
 		Produto p1 = new Produto(null, "computador", 2000);
 		Produto p2 = new Produto(null, "impressora", 800);
 		Produto p3 = new Produto(null, "mouse", 80);
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().add(p2);
-		
+
 		p1.getCategorias().add(cat1);
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().add(cat1);
-		
+
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
-		
-		
-		Estado est1 = new Estado(null,"Minas Gerais");
-		Estado est2 = new Estado(null,"Sao Paulo");
-		
-		Cidade cid1 = new Cidade(null,"Uberlandia",est1);
-		Cidade cid2 = new Cidade(null,"Sao Paulo",est2);
-		Cidade cid3 = new Cidade(null,"Campinas",est2);
 
-		est1.getCidades().addAll(Arrays.asList(cid1));
-		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
-		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 		estadoRepo.saveAll(Arrays.asList(est1, est2));
-		cidadeRepo.saveAll(Arrays.asList(cid1, cid2, cid3));
+		cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
 
-		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 	}
 }
---
+++ src/main/java/com/qsl/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,36 @@
+package com.qsl.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOA_FISICA(1, "Pessoa Fisica"),
+	PESSOA_JURIDICA(2, "Pessoa Juridica");
+	
+	private int cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public static TipoCliente toEnum(Integer cod) {
+		if (cod==null) {
+			return null;
+		}
+		for (TipoCliente tp : TipoCliente.values()) {
+			if (cod.equals(tp.getCod())) {
+				return tp;
+			}
+		}
+		throw new IllegalArgumentException("id invalido : " + cod);
+	}
+}
