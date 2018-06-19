e42b7d41da446003b30fbe5d980da74b9c26a22d
Ricardo Scussel <scussel.ricardo@gmail.com>
2018-06-19 16:50:48 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/qsl/cursomc/services/CategoriaService.java
+++ src/main/java/com/qsl/cursomc/services/CategoriaService.java
@@ -42,8 +42,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -67,4 +68,7 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 			return new Categoria(objDto.getId(), objDto.getNome());
 	}
+	private void updateData(Categoria newObj, Categoria obj) {
+				newObj.setNome(obj.getNome());
+		}
 }
